bfd7a5918e1d5a2f161688d15913a00c203b99d8
MrLinZHui <742847492@qq.com>
2019-07-13 12:21:33 +0800

    should_get_right_car_when_give_a_ticket

--- src/main/java/com/thoughtworks/tdd/Story1/AC2/ParkingLot.java
+++ src/main/java/com/thoughtworks/tdd/Story1/AC2/ParkingLot.java
@@ -1,11 +1,33 @@
 package com.thoughtworks.tdd.Story1.AC2;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParkingLot {
+    private HashMap<String,Car> carMap;
+    public ParkingLot() {
+        carMap = new HashMap<>();
+    }
+
     public Ticket park(Car car) {
-        return new Ticket();
+        Ticket ticket = new Ticket();
+        carMap.put(ticket.getTicketnum(),car);
+        return ticket;
     }
 
     public Car getCar(Ticket ticket) {
-        return new Car();
+        if
+        Car car = carMap.get(ticket.getTicketnum());
+        carMap.remove(ticket.getTicketnum());
+        return car;
+    }
+
+    public HashMap<String ,Car> getCarMap() {
+        return carMap;
+    }
+
+    public void setCarMap(HashMap<String, Car> carMap) {
+        this.carMap = carMap;
     }
 }
